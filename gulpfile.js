67375b48eb528f05de6dfd66d575c515371160ea
Grigør <g@yvn.io>
2017-02-01 17:37:42 +0400

    RO-8 #updated gulpfile.js for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,3 @@
-// import gulp from 'gulp';
-// import gulpLoadPlugins from 'gulp-load-plugins';
-// import path from 'path';
-// import del from 'del';
-// import runSequence from 'run-sequence';
-// import babelCompiler from 'babel-core/register';
-// import * as isparta from 'isparta';
-
-// const plugins = gulpLoadPlugins();
-
-// const paths = {
-// 	js: ['./**/*.js', '!dist/**', '!node_modules/**', '!coverage/**'],
-// 	nonJs: ['./package.json', './.gitignore'],
-// 	tests: './src/tests/*.js',
-// };
-
-// const options = {
-// 	codeCoverage: {
-// 		reporters: ['lcov', 'text-summary'],
-// 		thresholds: {
-// 			global: { statements: 80, branches: 80, functions: 80, lines: 80 },
-// 		},
-// 	},
-// };
-
-
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
