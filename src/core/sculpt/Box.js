6039f0712b50009f53d1414be965c0efd64309b3
Gor Matevosyan <gormatevosyan14@gmail.com>
2017-02-16 16:33:32 +0400

    RO-270 # added comments

--- src/core/sculpt/Box.js
+++ src/core/sculpt/Box.js
@@ -2,6 +2,10 @@ import {Sculpt} from './Sculpt';
 
 /**
  * Simple Box
+ * @param width {number} box width
+ * @param height {number} box height
+ * @param depth {number} box depth
+ * @param material {THREE.Material}
  */
 export class Box extends Sculpt {
     constructor(width = .5, height = .5, depth = .5, material = new THREE.MeshBasicMaterial({color: 336699})) {
