ffafa43a8284f7ac4f4bd87082900a4619e49102
Gor Matevosyan <gormatevosyan14@gmail.com>
2017-03-14 16:00:02 +0400

    RO-495 # added argument handler for Sphere and Box

--- src/core/sculpt/Sphere.js
+++ src/core/sculpt/Sphere.js
@@ -1,4 +1,5 @@
 import {Sculpt} from './Sculpt';
+import {AScheme} from '../utils';
 
 /**
  * Simple Sphere
@@ -7,9 +8,27 @@ import {Sculpt} from './Sculpt';
  * @param heightSegments {number}
  * @param material {THREE.Material}
  */
+
+const constructorScheme = {
+    radius: AScheme.number().default(.2),
+    widthSegments: AScheme.number().default(10),
+    heightSegments: AScheme.number().default(10),
+    material: AScheme.any().hasProperty('isMaterial').default(() => new THREE.MeshBasicMaterial({color: 336699}))
+};
+
 export class Sphere extends Sculpt {
-    constructor(radius = .2, widthSegments = 10, heightSegments = 10, material = new THREE.MeshBasicMaterial({color: 336699})) {
-        const threeSphere = new THREE.Mesh(new THREE.SphereGeometry(radius, widthSegments, heightSegments), material);
+    constructor(...args) {
+        args = AScheme.validate(args, constructorScheme);
+
+        const threeSphere = new THREE.Mesh(new THREE.SphereGeometry(args.radius, args.widthSegments, args.heightSegments), args.material);
         super(threeSphere);
+
+        this._radius = args.radius;
     }
+
+    get radius() {
+        return this._radius;
+    }
+
+    // todo: add setter for radius
 }
--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -3,12 +3,13 @@ import * as object from './object';
 import * as Enum from './enum';
 import * as utils3D from './3Dutils';
 import * as threeWrapper from './threeWrappers';
-import * as AScheme from './AScheme';
+
 export {
     string,
     object,
-    AScheme,
     Enum,
     utils3D,
     threeWrapper
 }
+
+export * from './AScheme';
